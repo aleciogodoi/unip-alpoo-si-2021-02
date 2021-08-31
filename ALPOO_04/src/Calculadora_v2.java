f1dbfffb33e06fb658b8fd30e05e91d6ac44b603
Alécio <alecio.godoi@ymail.com>
2021-08-31 19:55:26 -0300

    Calculadora_V2

---
+++ ALPOO_04/src/Calculadora_v2.java
@@ -0,0 +1,9 @@
+
+public class Calculadora_v2 extends TelaBase {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ ALPOO_04/src/TelaBase.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class TelaBase extends JFrame {
+	TelaBase(){
+		setTitle("Tela Base");
+		setSize(500, 300);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+}

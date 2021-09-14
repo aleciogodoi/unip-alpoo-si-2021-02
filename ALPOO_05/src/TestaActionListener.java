23a8bcfadc4a1ac6a1cec3dd73737610ff2fd492
Alécio <alecio.godoi@ymail.com>
2021-09-14 20:01:33 -0300

    ALPOO 05

--- ALPOO_05/src/TestaActionListener.java
+++ ALPOO_05/src/TestaActionListener.java
@@ -1,8 +1,10 @@
+import java.awt.Color;
 import java.awt.FlowLayout;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
-public class TestaActionListener extends TelaBase {
+public class TestaActionListener extends TelaBase implements ActionListener{
 
 	JButton btVermelho = new JButton("Vermelho");
 	JButton btAzul = new JButton("Azul");
@@ -17,9 +19,30 @@ public class TestaActionListener extends TelaBase {
 		this.add(btVerde);
 		this.add(btAmarelo);
 		this.add(btSair);
+		this.setVisible(true);
+		this.btVermelho.addActionListener(this);
+		this.btAzul.addActionListener(this);
+		this.btVerde.addActionListener(this);
+		this.btAmarelo.addActionListener(this);
+		this.btSair.addActionListener(this);
 	}
 	public static void main(String[] args) {
 		TestaActionListener t01 = new TestaActionListener();
-		t01.setVisible(true);
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btVermelho) {
+			this.getContentPane().setBackground(Color.red);
+		} else if (e.getSource() == btAzul) {
+			this.getContentPane().setBackground(Color.blue);			
+		} else if (e.getSource() == btVerde) {
+			this.getContentPane().setBackground(Color.green);			
+		} else if (e.getSource() == btAmarelo) {
+			this.getContentPane().setBackground(Color.yellow);			
+		} else if (e.getSource() == btSair) {
+			System.exit(0);			
+		}
+		
 	}
 }

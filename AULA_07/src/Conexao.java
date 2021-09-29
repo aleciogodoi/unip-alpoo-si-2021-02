87ef4322e5d9ff5a56b8595e1bc5b6489c04a830
Alécio <alecio.godoi@ymail.com>
2021-09-28 21:11:18 -0300

    Aula 07

--- AULA_07/src/Conexao.java
+++ AULA_07/src/Conexao.java
@@ -5,6 +5,8 @@ public class Conexao {
 		try {
 			Connection conn;  // objeto para conexão
 			Class.forName("org.sqlite.JDBC"); 
+			System.out.println("Driver JDBC encontrado! ");
+
 		} catch(ClassNotFoundException ex) {
 			System.out.println("Driver JDBC não encontrado! "+ ex);
 			System.exit(0);

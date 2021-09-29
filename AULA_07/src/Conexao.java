0ecc3791b3114948303232473fcc21a0407538b4
Alécio <alecio.godoi@ymail.com>
2021-09-28 21:25:34 -0300

    Aula 07

--- AULA_07/src/Conexao.java
+++ AULA_07/src/Conexao.java
@@ -1,4 +1,6 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexao {
 	public Connection conectar() {
@@ -6,15 +8,31 @@ public class Conexao {
 			Connection conn;  // objeto para conexão
 			Class.forName("org.sqlite.JDBC"); 
 			System.out.println("Driver JDBC encontrado! ");
+			
+			String pathBD = System.getProperty("user.dir").replace("\\", "/");
+			String bdUrl = "jdbc:sqlite:"+pathBD + "/bd/ALPOO.db";
+			
+			conn  = DriverManager.getConnection(bdUrl);
 
+			System.out.println("Conexão realizada! "+conn);
+			return conn;
 		} catch(ClassNotFoundException ex) {
 			System.out.println("Driver JDBC não encontrado! "+ ex);
 			System.exit(0);
-		}
-		return null;
+			return null;
+		} catch (SQLException ex) {
+			System.out.println("Problemas na conexão com a base de dados! "+ ex);
+			System.exit(0);	
+			return null;
+		} 
 	}
 	
 	public void fecharConexao(Connection conn) {
-		
+		try {
+			conn.close();
+			System.out.println("Conexão Fechada! "+ conn);
+		} catch (SQLException e) {
+			System.out.println("Problemas na conexão com a base de dados! "+ e);
+		}
 	}
 }
